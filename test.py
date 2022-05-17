0e27a24574b4e25e7b23333cdca8113cca9fefc1
arvind2806 <arvind2806@gmail.com>
2022-05-17 16:18:19 +0530

    pizza

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ print("this is my first time in git")
 event = input("enter hungry or thirsty:\n")
 if event == 'hungry':
     print("Eat samosa")
+    print("Eat Pizza")
 
 elif event == 'thirsty':
     print("Drink water")
