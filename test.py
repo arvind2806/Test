9a249598e5291e020e204a8272aeb04b2c8cf810
arvind2806 <arvind2806@gmail.com>
2022-05-17 15:28:11 +0530

    samosa

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
 print("this is my first time in git")
+
+event = input("enter hungry or thirsty:\n")
+if event == 'hungry':
+    print("Eat samosa")
+
+elif event == 'thirsty':
+    print("Drink water")
